c3ea8c7578da9b14c1b96440dcb2f2c81782f489
Yuki Nagae <yuki.nagae1130@gmail.com>
2017-05-15 21:27:39 +1000

    add some comments

--- bubblepop/SettingViewController.swift
+++ bubblepop/SettingViewController.swift
@@ -10,60 +10,46 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
+// show and mamnage setting
 class SettingViewController: UIViewController {
 
     @IBOutlet weak var maxBubbleSlider: UISlider!
-
-    @IBOutlet weak var gameTimeSlider: UISlider!
-
     @IBOutlet weak var maxBubbleLabel: UILabel!
 
+    @IBOutlet weak var gameTimeSlider: UISlider!
     @IBOutlet weak var gameTimeLabel: UILabel!
-
-    @IBAction func sliderChanged(_ sender: Any) {
-
-        if let slider = sender as? UISlider {
-            let currentValue = Int(slider.value)
-            maxBubbleLabel.text = "\(currentValue)"
-            UserDefaults.standard.set(currentValue, forKey: "MaxBubbles")
-        }
-    }
-
-    @IBAction func gameTimeChanged(_ sender: Any) {
-        if let slider = sender as? UISlider {
-            let currentValue = Int(slider.value)
-            gameTimeLabel.text = "\(currentValue)"
-            UserDefaults.standard.set(currentValue, forKey: "GameTime")
-        }
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // max bubble
         let maxBubble = UserDefaults.standard.integer(forKey: "MaxBubbles")
-
-        if maxBubble > 0 {
-            maxBubbleLabel.text = "\(maxBubble)"
-            maxBubbleSlider.value = Float(maxBubble)
-        } else {
-            let maxBubblesDefaultValue = 15
-            maxBubbleLabel.text = "\(maxBubblesDefaultValue)"
-            maxBubbleSlider.value = Float(maxBubblesDefaultValue)
-        }
+        maxBubbleLabel.text = "\(maxBubble)"
+        maxBubbleSlider.value = Float(maxBubble)
 
         // game time
         let gameTime = UserDefaults.standard.integer(forKey: "GameTime")
+        gameTimeLabel.text = "\(gameTime)"
+        gameTimeSlider.value = Float(gameTime)
+        
+    }
 
-        if gameTime > 0 {
-            gameTimeLabel.text = "\(gameTime)"
-            gameTimeSlider.value = Float(gameTime)
-        } else {
-            let gameTimeDefaultValue = 60
-            gameTimeLabel.text = "\(gameTimeDefaultValue)"
-            gameTimeSlider.value = Float(gameTimeDefaultValue)
+    // when max bubble slider changed
+    @IBAction func maxBubbleChanged(_ sender: Any) {
+        if let slider = sender as? UISlider {
+            let currentValue = Int(slider.value)
+            maxBubbleLabel.text = "\(currentValue)"
+            UserDefaults.standard.set(currentValue, forKey: "MaxBubbles") // update max bubble setting
         }
+    }
 
+    // when game time slider changed
+    @IBAction func gameTimeChanged(_ sender: Any) {
+        if let slider = sender as? UISlider {
+            let currentValue = Int(slider.value)
+            gameTimeLabel.text = "\(currentValue)"
+            UserDefaults.standard.set(currentValue, forKey: "GameTime") // update game time setting
+        }
     }
 
     override var shouldAutorotate: Bool {
