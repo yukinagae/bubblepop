b506ace5391f65d6b37dce49b3c78a17a0359196
Yuki Nagae <yuki.nagae1130@gmail.com>
2017-05-15 19:55:54 +1000

    scoreboard: remove table view controller and add view controller with table view

--- bubblepop/ScoreboardViewController.swift
+++ bubblepop/ScoreboardViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class ScoreboardViewController: UITableViewController {
+class ScoreboardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
 
     var scores: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
 
         if let scs = UserDefaults.standard.object(forKey: "scores") as? Dictionary<String, UInt32> {
             for (key, value) in scs {
@@ -24,16 +28,16 @@ class ScoreboardViewController: UITableViewController {
         }
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return scores.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
         cell.textLabel?.text = scores[indexPath.row]
 
